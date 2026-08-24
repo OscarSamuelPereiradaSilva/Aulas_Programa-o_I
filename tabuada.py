3d80cd7088272a7864a8b75613e0983b2b07e594
my-name <ospsilva7@gamil.com>
2026-08-24 10:54:56 -0300

    Tabuada Atualizada

--- tabuada.py
+++ tabuada.py
@@ -3,5 +3,5 @@ while continuar:
     numero = int(input("Digite um número para ver a tabuada: "))
     for i in range(1,11):
         print(f"{numero} x {i} = {numero * i}")
-    continuar = input("Deseja continuar? (s/n) ")
+    continuar = input("Deseja continuar? (s/n) ").lower().strip()
     continuar = True if continuar == "s" else False 
